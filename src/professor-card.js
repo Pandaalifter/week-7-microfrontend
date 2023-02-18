762d7c1a9e8ddf1de96de8b3891165e26915a0bc
Pandaa <96748487+Pandaalifter@users.noreply.github.com>
2023-02-18 11:55:56 -0800

    Hackermanz
    Hackerplanz

--- src/professor-card.js
+++ src/professor-card.js
@@ -1,21 +1,26 @@
 import { LitElement, html, css } from 'lit';
 import "@lrnwebcomponents/meme-maker/meme-maker.js"; 
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+const chad = new URL('../assets/the-moment.png', import.meta.url).href;
 
 class ProfessorCard extends LitElement {
   static properties = {
-    name: { type: String, reflect: true },
+    title: {type: String, reflect: true},
+    subtitle: {type: String},
+    chadLabel: {type: String},
+    top: {type: String},
+    bottom: {type: String}
   }
 
   static styles = css`
+    :host{
+      display: inline-block;
+    }
     .card{
       border-radius: 24px;
       border: 3px solid #041E42;
       padding: 8px;
       width: 400px;
-      display: block;
-      margin: auto;
       position: relative;
       overflow: hidden;
       box-shadow: 0px 8px 24px #999;
@@ -71,38 +76,19 @@ class ProfessorCard extends LitElement {
                       1px -1px 0 #000,
                       -1px -1px 0 #000;
     }
+    .subtitle{
+      color: white;
+      font-size: 16px;
+      text-shadow: -1px 1px 2px #000,
+                      1px 1px 2px #000,
+                      1px -1px 0 #000,
+                      -1px -1px 0 #000;
+    }
     .giaImg{
       width: 200px;
       border-radius: 24px;
       box-shadow: 0px 10px 24px #999;
       float: left;
-    }
-    .btnSpacing{
-      display: inline-block;
-    }
-    .outsideBtn{
-      color: white;
-      background-color: #041E42;
-      border-radius: 5px;
-      font-size: 20px;
-      width: 120px;
-      height: 50px;
-      box-shadow: 0px 10px 24px #999;
-      border: 1px solid #041E42;
-      margin-right: 32px;
-      margin-bottom: 16px;
-    }
-    .outsideBtn:hover{
-      color: #e0e0e0;
-      background-color: #07377a;
-      box-shadow: 0px 10px 24px white;
-      border: 1px solid #07377a;
-    }
-    .outsideBtn:focus{
-      color: #e0e0e0;
-      background-color: #07377a;
-      box-shadow: 0px 10px 24px white;
-      border: 1px solid #07377a;
     }
     .basic{
       background-color: hotpink;
@@ -111,24 +97,48 @@ class ProfessorCard extends LitElement {
       border: 3px solid purple;
       box-shadow: 0px 8px 24px yellow;
     }
+
+    @media only screen and (min-width: 800px) and (max-width: 1200px){
+      .card{
+        background-color: maroon;
+      }
+    }
+    @media only screen and (max-width: 500px){
+      .card{
+        background-color: lightblue;
+      }
+    }
   `;
 
   constructor() {
     super();
-    this.header = 'My app';
+    this.title = "Professor Giacobe";
+    this.subtitle = "Chad of Cyber IST";
+    this.chadLabel = "Details";
+    this.top = "Giacobe";
+    this.bottom = "Double Overlaid";
   }
 
   render() {
     return html`
       <div class="card">
-        <h1 class="title">Chad of Cyber IST</h1>
+        <h1 class="title">${this.title}</h1>
+        <h2 class="subtitle">${this.subtitle}</h2>
 
-        <img class="giaImg" src="https://media.discordapp.net/attachments/963095262363017246/1020131830323744788/unknown.png?width=468&height=468" alt="Professor Giacobe"/>
+        <div class="giaImg">
+          <meme-maker 
+            image-url="${chad}" 
+            top-text="${this.top}" 
+            bottom-text="${this.bottom}">
+          </meme-maker>
+        </div>
 
         <div class="textbox">
           <details>
-            <summary class="haxbtn">Details</summary>
-            <p class="description pScale">Professor Giacobe may look like a simple man but perceptions can be misleading. Underneath the facade of a simple college of IST professor lies the holiness of an <strong>IST GOD</strong>!</p>
+            <summary class="haxbtn">${this.chadLabel}</summary>
+            <div class="description">
+              <slot></slot>
+            </div>
           </details>
 
         </div>
